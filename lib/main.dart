93829c24ac78661efba6700d9b1d8cc384487d68
Marcelo Romero <marceloiromero@gmail.com>
2021-12-19 18:00:12 -0300

    Fix en funcionalidad de atrjeta de creditos

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutterito´s'),
     );
   }
